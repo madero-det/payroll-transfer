d73d1cfbd6a1ef326836b5f6135300ea50577bf9
Madero Det <detmadero@gmail.com>
2024-11-14 05:52:22 +0000

    Update

--- src/test/java/com/mcnc/payroll/DynamicClass.java
+++ src/test/java/com/mcnc/payroll/DynamicClass.java
@@ -2,57 +2,13 @@ package com.mcnc.payroll;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mcnc.payroll.model.MData;
-import com.mcnc.payroll.model.Property;
-import com.mcnc.payroll.model.ValidationRule;
-import com.mcnc.payroll.util.DynamicEntity;
-
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validator;
+import com.mcnc.payroll.service.ValidatePropertyService;
 
 public class DynamicClass {
 
-	private static Validator validator;
-
 	public static void main(String[] args) throws Exception {
-		// Initialize validator (typically injected in Spring)
-		try (LocalValidatorFactoryBean factoryBean = new LocalValidatorFactoryBean()) {
-			factoryBean.afterPropertiesSet();
-			validator = factoryBean.getValidator();
-		}
-		// Mock metadata from the database
-		List<Property> properties = List.of(
-			new Property("TRN10100521", "withdrawalAccountNo", "", "string", "request", true, List.of(
-				new ValidationRule("notnull", "", "Withdrawal account number cannot be null."),
-				new ValidationRule("notempty", "", "Withdrawal account number must not be empty.")
-			), null),
-			new Property("TRN10100521", "transactionCurrencyCode", "", "string", "request", true, List.of(
-				new ValidationRule("notnull", "", "Transaction currency code cannot be null."),
-				new ValidationRule("notempty", "", "Transaction currency code cannot be empty."),
-				new ValidationRule("pattern", "^[USD|KHR]{3}$", "Transaction currency code must be USD or KHR.")
-			), null),
-			new Property("TRN10100521", "transferList", "", "list", "request", true, List.of(
-				new ValidationRule("valid", "", "Transfer list is invalid."),
-				new ValidationRule("size", "1,10", 	"Transfer list must have between 1 and 10 items.")
-			), List.of(
-				new Property("TRN10100521", "recipientAccountNo", "transferList", "string", "request", true, List.of(
-					new ValidationRule("notnull", "", "Recipient account number cannot be null."),
-					new ValidationRule("notempty", "", "Recipient account number must not be empty.")
-				), null),
-				new Property("TRN10100521", "transactionAmount", "transferList", "bigdecimal", "request", true, List.of(
-					new ValidationRule("notnull", "", "Transaction amount cannot be null."),
-					new ValidationRule("decimalmin", "0.01", "Transaction amount must be greater than or equal to 0.01")
-				), null)
- 			))
-		);
-
-		// Generate the dynamic class
-		Class<?> dynamicClass = DynamicEntity.generate(properties);
 
 		List<MData> transferList = new ArrayList<>();
 		MData itemTransfer = new MData();
@@ -65,18 +21,8 @@ public class DynamicClass {
 		data.put("transactionCurrencyCode", "THB");
 		data.put("transferList", transferList);
 
-		// Set field values dynamically
-		ObjectMapper mapper = new ObjectMapper();
-		Object instance = mapper.convertValue(data, dynamicClass);
+		ValidatePropertyService validatePropertyService = new ValidatePropertyService();
+		validatePropertyService.validateProperty(data);
 
-		// Validate the dynamic instance
-		validateDynamicInstance(instance);
-	}
-
-	private static void validateDynamicInstance(Object instance) {
-		Set<ConstraintViolation<Object>> violations = validator.validate(instance);
-		for (ConstraintViolation<Object> violation : violations) {
-			System.out.println(violation.getMessage());
-		}
 	}
 }
